b4c9cd6e1ec63e3f52f584ca81e787ac962a89cc
MoisesFGNeto <117291517+MoisesFGNeto@users.noreply.github.com>
2023-07-21 04:21:47 +0200

    Webhook fixed

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,6 +1,7 @@
 import { mongooseConnect } from "@/lib/mongoose";
 import { Order } from "@/models/Order";
 const stripe = require('stripe')(process.env.STRIPE_SK);
+import { buffer } from "micro";
 
 const endpointSecret = "whsec_a5ef1bb4e884cef3c20c970ca1f6e9bec4049510e3b6ceda3c38a9af7efdd238";
 
@@ -8,11 +9,11 @@ export default async function handler(req, res) {
   await mongooseConnect();
 
   const sig = req.headers['stripe-signature'];
-
+  const secret = process.env.STRIPE_SK;
   let event;
 
   try {
-    event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
+    event = stripe.webhooks.constructEvent(await buffer(req), sig, secret);
   } catch (err) {
     res.status(400).send(`Webhook Error: ${err.message}`);
     return;
