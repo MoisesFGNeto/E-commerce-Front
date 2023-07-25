4cc18329e81220a4111aa43065d63f8a3274f9fc
MoisesFGNeto <117291517+MoisesFGNeto@users.noreply.github.com>
2023-07-25 22:46:54 +0200

    hamburger menu

--- components/icons/Bars.js
+++ components/icons/Bars.js
@@ -1,6 +1,6 @@
-export default function BarsIcon({className="w-6 h-6"}) { 
+export default function BarsIcon() { 
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" style={{width:'30px'}}>
       <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
     </svg>
 
